cc137c1bedcb3882c6b88f552ed5f38499a52205
gastonlcruz <gastonlcruz@gmail.com>
2024-02-09 01:00:44 -0300

    expf

--- src/components/Experiencia/Experiencia.jsx
+++ src/components/Experiencia/Experiencia.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 const Experiencia = () => {
   return (
+    <div>
     <div className="experiencia">
         <div className="izq">
             <div className="trabajo">
@@ -23,8 +24,55 @@ const Experiencia = () => {
             <div className="linea">
                 <div className="circulo"></div>
             </div>
-
         </div>
+        <div style={{"width":"33.333333%"}}></div>
+       
+    </div>
+    <div className="experiencia">
+        <div style={{"width":"33.333333%"}}></div>
+            <div className="centro">
+                <div className="linea">
+                    <div className="circulo"></div>
+                </div>
+            </div>
+        <div className="izq">
+            <div className="trabajo">
+                Programador
+            </div>
+            <div className="trabajodesc">
+                trabajo hace 8 años
+            </div>
+            <div className="fecha">
+                2015-2023
+            </div>
+            <div className="empresa">
+                ARMADA ARGENTINA
+            </div>
+        </div>
+    </div>
+    <div className="experiencia">
+        <div className="izq">
+            <div className="trabajo">
+                Programador
+            </div>
+            <div className="trabajodesc">
+                trabajo hace 8 años
+            </div>
+            <div className="fecha">
+                2015-2023
+            </div>
+            <div className="empresa">
+                ARMADA ARGENTINA
+            </div>
+        </div>
+        <div className="centro">
+            <div className="linea">
+                <div className="circulo"></div>
+            </div>
+        </div>
+        <div style={{"width":"33.333333%"}}></div>
+       
+    </div>
     </div>
   )
 }
