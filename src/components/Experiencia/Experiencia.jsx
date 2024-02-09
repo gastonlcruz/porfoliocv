36f030b5832ea1b5da14e79a4920b2cbd897bb7b
gastonlcruz <gastonlcruz@gmail.com>
2024-02-09 17:54:48 -0300

    expanimation

--- src/components/Experiencia/Experiencia.jsx
+++ src/components/Experiencia/Experiencia.jsx
@@ -1,10 +1,28 @@
 import "./experiencia.css";
-
-import React from 'react'
+import { motion, useInView, useScroll } from "framer-motion";
+import { useRef } from "react";
 
 const Experiencia = () => {
+    const experienciaRef= useRef();
+    const isExperienceRefInView = useInView(experienciaRef, { margin: "-100px" });
+    
+
+
   return (
-    <div>
+    <div className="move" ref={experienciaRef}>
+        <motion.h1
+              initial={{ x: "-300px" }}
+              animate={isExperienceRefInView ? { x: "350px" } : {}}
+              transition={{ delay: 0.2 }}
+              className="exp"
+            >
+              EXPERIENCE
+            </motion.h1>
+            <motion.div
+              initial={{ x: "-300px" }}
+              animate={isExperienceRefInView ? { x: "218px" } : {}}
+              className=""
+            >
     <div className="experiencia">
         <div className="izq">
             <div className="trabajo">
@@ -73,6 +91,7 @@ const Experiencia = () => {
         <div style={{"width":"33.333333%"}}></div>
        
     </div>
+    </motion.div>
     </div>
   )
 }
